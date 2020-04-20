6247c40d549c6e1ab5a03383ec797444c73f42bb
Thomas Banks <thomasxbanks@gmail.com>
2020-04-20 07:37:55 +0100

    Adds hero image

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,28 @@
 <script>
   export let name;
   import Header from './Header.svelte'
+  import Image from './Image.svelte'
 </script>
 
 <style>
+  main {
+    max-width: calc(2rem + 67ch);
+    margin-right: auto;
+    margin-left: auto;
+    padding: 1rem;
+  }
 
+  main>*+* {
+    margin-top: 1.5em;
+  }
 </style>
 
 <svelte:head>
   <title>Svelte Lunch & Learn</title>
 </svelte:head>
 
+<Header hero={true} {name} />
+<Image src="http://placekitten.com/1920/768" alt="A lovely kitten" />
 <main>
-  <Header hero={true} {name} />
+  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestias officiis, ipsam voluptatem porro reprehenderit quasi cum maxime natus! Ipsum rerum modi ullam, magnam a placeat aperiam excepturi iure iusto repellendus?</p>
 </main>
---
+++ src/Image.svelte
@@ -0,0 +1,10 @@
+<script>
+  export let src;
+  export let alt;
+</script>
+
+<style></style>
+
+<figure>
+  <img src={src} alt={alt}>
+</figure>
