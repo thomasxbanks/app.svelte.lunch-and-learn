126823b57b6bc78fb291b08e2d88a378af92077b
Thomas Banks <thomas.banks@havaslynx.com>
2020-04-30 15:52:10 +0100

    Adds "Load more" button

---
+++ src/Button.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let size = "regular";
+  export let variant = "default";
+
+  const classes = `${size} ${variant}`;
+</script>
+
+<style>
+  button {
+    display: inline-block;
+    padding: 0.5em 1.5em;
+    border-width: 0.125em;
+    border-style: solid;
+    border-radius: 0.25em;
+    line-height: 1em;
+  }
+  .default {
+    background-color: tomato;
+    color: white;
+  }
+  .ghost {
+    border-color: tomato;
+    color: tomato;
+  }
+  .small {
+    font-size: 0.75rem;
+  }
+  .regular {
+    font-size: 1rem;
+  }
+  .large {
+    font-size: 1.25rem;
+  }
+</style>
+
+<button class={classes} on:click>
+  <slot />
+</button>
--- src/Posts.svelte
+++ src/Posts.svelte
@@ -1,24 +1,40 @@
 <script>
-  import Card from './Card.svelte';
-
+  import Button from "./Button.svelte";
+  import ButtonContainer from "./ButtonContainer.svelte";
+  import Card from "./Card.svelte";
+  let loading = false;
+  let page = 1;
   let posts = [];
-  import { onMount } from 'svelte'
+  import { onMount } from "svelte";
 
-  const getPosts = async () => {
-    const endpoint = "http://scrummable.com/wp-json/wp/v2/posts?_embed"
-    const raw = await fetch(endpoint)
-    const response = await raw.json()
-    console.log(response)
-    return response;
-  }
+  const getPosts = async (page = 1) => {
+    loading = true;
+    const endpoint = `http://scrummable.com/wp-json/wp/v2/posts?_embed&per_page=3&page=${page}`;
+    const raw = await fetch(endpoint);
+    const response = await raw.json();
+    console.log(response);
+    posts = [...posts, ...response];
+    loading = false;
+  };
 
   onMount(async () => {
-    posts = await getPosts()
-  })
+    await getPosts();
+  });
 </script>
 
 <style>
-
+  .grid {
+    max-width: 1200px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  section {
+    width: max-content;
+    margin-right: auto;
+    margin-left: auto;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+  }
 </style>
 
 <section class="grid">
@@ -26,4 +42,11 @@
   {#each posts as post, index}
     <Card {post} {index} />
   {/each}
+</section>
+<section>
+  <ButtonContainer>
+    <Button size="large" on:click={() => getPosts(++page)}>
+      {loading ? 'Loading...' : 'Load more'}
+    </Button>
+  </ButtonContainer>
 </section>
